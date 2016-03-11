fa31a3722c4158649415cd3f6c631738b18ba35a
Kevin van der Vlist <kvdvlist@sogyo.nl>
2016-03-11 10:02:06 +0100

    Complete example

---
+++ complete/autocomplete.js
@@ -0,0 +1,48 @@
+(function (global, $, Rx) {
+  
+  function search(term) {
+    return Rx.Observable
+      .fromPromise($.ajax({
+        url: 'http://localhost:8000/s',
+        data: {
+          search: term
+        }
+      }).promise());
+  }
+
+  function main() {
+    demo('Complete', 'A complete Rx example.');
+    
+    var $input = $('#searchbox');
+    var $results = $('#results');
+
+    // Get all distinct key up events from the input and only fire if long enough and distinct
+    var keyup = Rx.Observable.fromEvent($input, 'keyup')
+      .map(function (e) {
+        return e.target.value; // Project the text from the input
+      })
+      .filter(function (text) {
+        return text.length >= 2;
+      })
+      .debounce(750 /* Pause for 750ms */ )
+      .distinctUntilChanged(); // Only if the value has changed
+
+    keyup
+      .flatMapLatest(search)
+      .doOnNext(function() {
+        $results.empty();
+      })
+      .flatMap(function(words) {
+        return words;
+      })
+      .subscribe(
+        function (word) {
+          $results.append($('<li>').text(word));
+        },
+        function (error) {
+          console.error(error);
+        });
+  }
+
+  $(main);
+}(window, jQuery, Rx));
